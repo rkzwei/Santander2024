90faedc2ad69495c0121ea0cb32abe82266eda21
rkzwei <rk@rkzwei.dev>
2024-05-21 15:07:45 -0300

    Exemplos para ter no futuro

---
+++ Pasta de Treino/src/ExemploForArrays.java
@@ -0,0 +1,13 @@
+public class ExemploForArrays {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for(int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        for(int i = 0; i < 10; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
+
